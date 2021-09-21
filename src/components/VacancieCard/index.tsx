f94657bfcee29efa6e45e7e18270aef125b948cc
souzjfe <souzajefison@gmail.com>
2021-09-20 22:45:29 -0300

    ✨ - feat: Modal to find user for add on project

--- src/components/VacancieCard/index.tsx
+++ src/components/VacancieCard/index.tsx
@@ -1,20 +1,26 @@
-import React, { HTMLAttributes, useContext, useEffect, useState } from 'react'
-import { BodyCard, DropdownList } from './styles'
-import { Link } from 'react-router-dom'
+import React, {
+  HTMLAttributes,
+  useContext,
+  useEffect,
+  useState,
+  useCallback,
+  ChangeEvent,
+  FormEvent,
+} from 'react'
+import { BodyCard, DropdownList, StyleInput, ProfileButton } from './styles'
+import { Link, useHistory, useLocation } from 'react-router-dom'
 import userDefault from '../../assets/icon/user.svg'
+import lupa from '../../assets/icon/lupa.svg'
 import { AxiosError } from 'axios'
 import { AreaType } from '../UI/SelectArea'
 import { ToolType } from '../UI/SelectTools'
 import api from '../../services/api'
 import Button from '../UI/Button'
-import { GiHamburgerMenu } from 'react-icons/gi'
 import { TypeSituationVacancy } from '../Vacancy'
 import Alert from '../../utils/SweetAlert'
-import Swal from 'sweetalert2'
-import { Toast } from 'react-toastify/dist/components'
-import { toast } from 'react-toastify'
-import { showToast } from '../Toast/Toast'
 import { Context } from '../../context/AuthContext'
+import Modal from '../UI/Modal'
+import { showToast } from '../Toast/Toast'
 export interface IVacancyCard {
   projeto_id: number
   pessoa_id: number
@@ -52,11 +58,107 @@ interface ISituationVacancy {
     isAvaliable: boolean
   }
 }
+interface IModalProps {
+  open: boolean
+  setOpen(isOpen: boolean): void
+  selectedPeople: ProfileType
+  setSelectedPeople(people: ProfileType): void
+  vacancyId: number
+}
+const ModalFindPeople: React.FC<IModalProps> = props => {
+  const history = useHistory()
+  const location = useLocation()
+  const [searchKey, setSearchKey] = useState<string>('')
+  const [peoplesFounds, setPeoplesFounds] = useState<ProfileType[]>([])
+  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target
+    api.get(`/api/v1/pessoa/nome/${value}`).then(response => {
+      setPeoplesFounds(response.data)
+    })
+  }, [])
+  const handleSubmit = useCallback((event: FormEvent) => {
+    event.preventDefault()
+  }, [])
+  async function newPeople(profile: ProfileType) {
+    if (profile.id !== 0) {
+      console.log(profile)
+      const find = await Alert({
+        title: `${profile?.nome}`,
+        imageUrl: profile?.foto_perfil
+          ? `https://conectar.s3.sa-east-1.amazonaws.com/uploads/${profile?.foto_perfil}`
+          : userDefault,
+        confirmButtonText: 'Confirmar',
+        showCancelButton: true,
+        cancelButtonText: 'Cancelar',
+      })
+      if (find.isConfirmed) {
+        await api
+          .put(`api/v1/pessoa_projeto/${props.vacancyId}`, {
+            pessoa_id: profile?.id,
+          })
+          .then(() => {
+            showToast('success', 'Vaga atualizada com Sucesso!')
+            props.setSelectedPeople(profile)
+          })
+          .catch((err: AxiosError) => {
+            Alert({
+              title: `Erro: ${err.message}`,
+              text: 'Não foi possível atualizar a vaga, tente novamente!',
+              icon: 'error',
+            })
+            return err?.response?.data.detail
+          })
+        props.setOpen(false)
+      }
+    }
+  }
+
+  return (
+    <Modal {...props}>
+      <StyleInput onSubmit={handleSubmit}>
+        <button type="submit">
+          <img src={lupa} alt="botao de pesquisa" />
+        </button>
+
+        <input placeholder="Nome ou usuário" onChange={handleChange} />
+      </StyleInput>
+      {peoplesFounds
+        .filter((_, index) => index <= 10)
+        .map(profile => (
+          <ProfileButton
+            key={profile.id}
+            type="button"
+            onClick={() => {
+              newPeople(profile)
+            }}
+          >
+            <img
+              src={
+                profile?.foto_perfil
+                  ? `https://conectar.s3.sa-east-1.amazonaws.com/uploads/${profile?.foto_perfil}`
+                  : userDefault
+              }
+              alt={profile?.nome}
+            />
+            <aside>
+              <h2>{profile?.nome}</h2>
+              <p>@{profile?.usuario}</p>
+            </aside>
+          </ProfileButton>
+        ))}
+    </Modal>
+  )
+}
+
 const VacancieCard: React.FC<Props> = ({ vacancy, ...rest }) => {
   const { user } = useContext(Context)
   const [possibleDeal, setPossibleDeal] = useState(false)
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   const [finalizedDeal, setFinalizedDeal] = useState(false)
   const [profile, setProfile] = useState<ProfileType>({} as ProfileType)
+  const [selectedPeople, setSelectedPeople] = useState<ProfileType>(
+    {} as ProfileType,
+  )
   const [agreement, setAgreement] = useState<string>('')
   const [office, setOffice] = useState<string>('')
   const situation: ISituationVacancy = {
@@ -99,72 +201,7 @@ const VacancieCard: React.FC<Props> = ({ vacancy, ...rest }) => {
       denyButtonColor: `var(--green)`,
     })
     if (result.isDenied) {
-      Alert({
-        title: 'Insira o username',
-        input: 'text',
-        inputAttributes: {
-          autocapitalize: 'off',
-        },
-        icon: 'info',
-        inputPlaceholder: '@',
-        showCancelButton: true,
-        confirmButtonText: 'Buscar',
-        showLoaderOnConfirm: true,
-        preConfirm: login => {
-          return api
-            .get(`/api/v1/pessoas/${login}`)
-            .then(response => {  
-              console.log(response.data)
-              if (response.data == null)
-                Swal.showValidationMessage(
-                  `Usuário não encontrado`,
-                )
-              else if (response.data.usuario == user.usuario){
-                Swal.showValidationMessage(
-                  `Usuário não pode ser o dono do projeto`,
-                )
-              }
-              else return response.data
-            })
-            .catch(error => {
-              Swal.showValidationMessage(`Erro: ${error}`)
-            })
-        },
-        allowOutsideClick: () => !Swal.isLoading(),
-      }).then(async result => {
-        console.log('Result confirmed')
-
-        if (result.isConfirmed) {
-          console.log(result.value)
-          const find = await Alert({
-            title: `${result.value.nome}`,
-            imageUrl: result.value.foto_perfil
-              ? `https://conectar.s3.sa-east-1.amazonaws.com/uploads/${result.value?.foto_perfil}`
-              : userDefault, 
-            confirmButtonText: 'Confirmar',
-            showCancelButton: true,
-            cancelButtonText: 'Cancelar',
-          })
-          if (find.isConfirmed) {
-            await api
-              .put(`api/v1/pessoa_projeto/${vacancy.id}`, {
-                pessoa_id: result.value.id,
-              })
-              .then(() => {
-                showToast('success', 'Vaga atualizada com Sucesso!')
-                setProfile(result.value)
-              })
-              .catch((err: AxiosError) => {
-                Alert({
-                  title: `Erro: ${err.message}`,
-                  text: 'Não foi possível atualizar a vaga, tente novamente!',
-                  icon: 'error',
-                })
-                return err?.response?.data.detail
-              })
-          }
-        }
-      })
+      setModalIsOpen(true)
     }
     if (result.isConfirmed)
       api
@@ -185,11 +222,13 @@ const VacancieCard: React.FC<Props> = ({ vacancy, ...rest }) => {
           return err?.response?.data.detail
         })
   }
-  console.log(vacancy.situacao);
-  
-    useEffect(() => {
-    setPossibleDeal(vacancy.situacao? vacancy.situacao == 'ACEITO' : false)
-    setFinalizedDeal(vacancy.situacao? vacancy.situacao == 'FINALIZADO' : false)
+  console.log(vacancy.situacao)
+
+  useEffect(() => {
+    setPossibleDeal(vacancy.situacao ? vacancy.situacao === 'ACEITO' : false)
+    setFinalizedDeal(
+      vacancy.situacao ? vacancy.situacao === 'FINALIZADO' : false,
+    )
   }, [vacancy])
 
   useEffect(() => {
@@ -227,6 +266,13 @@ const VacancieCard: React.FC<Props> = ({ vacancy, ...rest }) => {
       status={situation[`${vacancy.situacao}`].status}
       {...rest}
     >
+      <ModalFindPeople
+        open={modalIsOpen}
+        setOpen={setModalIsOpen}
+        selectedPeople={profile}
+        setSelectedPeople={setProfile}
+        vacancyId={vacancy.id}
+      />
       <Link to={`/perfil/${profile?.usuario}`}>
         <img
           src={
@@ -246,14 +292,11 @@ const VacancieCard: React.FC<Props> = ({ vacancy, ...rest }) => {
 
       {/* <Button theme="primary">Ver currículo</Button> */}
       {/* <DropdownList IconButton={ */}
-      {
-        !finalizedDeal && (
+      {!finalizedDeal && (
         <Button onClick={FindPeople} theme="secondary">
           Nova busca
         </Button>
-        )
-      }
-      
+      )}
 
       {/* <li>Perfis similares</li>
         <li>Perfis interessados</li>
--- src/components/VacancieCard/styles.ts
+++ src/components/VacancieCard/styles.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components'
 import { TypeStatusVacancy } from '.'
 import Dropdown from '../UI/Dropdown'
 import { BodyButton as DropdownStyle } from '../UI/Dropdown/styles'
+import { DivModalWindow } from '../UI/Modal/styles'
 interface CardProps {
   isAvailable: boolean
   status?: TypeStatusVacancy
@@ -19,9 +20,82 @@ export const DropdownList = styled(Dropdown)`
     }
   }
 `
+export const ProfileButton = styled.button`
+  background: transparent;
+  border: none;
+  width: 90%;
+  height: 100%;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  justify-content: flex-start;
+  gap: 0.6rem;
+  margin: 0.4rem;
+  margin-left: 2rem;
+  > img {
+    border-radius: 50%;
+    border: solid 1px var(--borderDivision);
+    width: 2.4rem;
+    height: 2.4rem;
+    object-fit: cover;
+    object-position: center;
+  }
+  > aside {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    > h2 {
+      font-size: 1rem;
+      word-break: keep-all;
+    }
+    > p {
+      text-align: center;
+      color: var(--gray);
+      font-size: 0.8rem;
+    }
+  }
+  &:hover {
+    background: var(--backgroundElevation);
+  }
+`
+
+export const StyleInput = styled.form`
+  background: white;
+  box-shadow: var(--boxShadow);
+  height: 2.4rem;
+  width: 60%;
+  margin: 0.2rem 0 2rem;
+  padding: 0 0.2rem;
+  border-radius: 0.2rem;
+  display: flex;
+  align-items: center;
+  > input {
+    border: 0;
+    width: max-content;
+    background: transparent;
+    height: 2rem;
+    outline: 0;
+    padding: 0 0.6rem;
+    flex: 1;
+  }
+  > button {
+    display: flex;
+    align-items: center;
+    border: 0;
+    background: transparent;
+    padding: 0 0.4rem;
+    border-right: 2px solid var(--borderDivision);
+    cursor: pointer;
+    width: 2rem;
+    img {
+      width: 16px;
+    }
+  }
+`
 export const BodyCard = styled.li<CardProps>`
   width: 168px;
-
   border-radius: var(--borderRadius);
   border: solid 1px var(--borderDivision);
   display: flex;
@@ -51,19 +125,19 @@ export const BodyCard = styled.li<CardProps>`
       }
     }
   }
-  label {
+  > label {
     position: absolute;
     right: 0.6rem;
   }
-  img {
+  > img {
     border-radius: 50%;
     width: 50px;
     height: 50px;
   }
-  h2 {
+  > h2 {
     font-size: 1.4rem;
   }
-  h3,
+  > h3,
   h4 {
     font-size: 1rem;
   }
@@ -103,9 +177,12 @@ export const BodyCard = styled.li<CardProps>`
     font: 600 1rem Raleway;
     padding: 0.1rem 0.4rem;
   }
-  button {
+  > button {
     width: 100%;
     font-size: 0.8rem;
     height: 2rem;
+  }
+  ${DivModalWindow} {
+    min-height: 400px;
   }
 `
